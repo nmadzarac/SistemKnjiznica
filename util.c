8fd7508f1554b0e8249da1a5d3c4f2fed274559b
nmadzarac <nikola.madzarac@gmail.com>
2024-06-19 15:09:28 +0200

    Delete util.c

--- util.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include "header1.h"
-#include "header.h"
-
-void clearInputBuffer() {
-    int c;
-    while ((c = getchar()) != '\n' && c != EOF) {}
-}
